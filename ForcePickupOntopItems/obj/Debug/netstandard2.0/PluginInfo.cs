937cf354a5c74ea2f162656642bc028486374e9c
Brett Hewitson <bletch1971@hotmail.com>
2022-08-22 14:51:50 +1000

    Delete ForcePickupOntopItems/obj directory

--- ForcePickupOntopItems/obj/Debug/netstandard2.0/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace ForcePickupOntopItems
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "bletch.dinkum.forcepickupontopitems";
-        public const string PLUGIN_NAME = "bletch.dinkum.forcepickupontopitems";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
